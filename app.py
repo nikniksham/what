53c57fb35fc89e3ead10c90b332e07540536fc8a
Niki <57631867+nikniksham@users.noreply.github.com>
2022-08-29 17:49:09 +0300

    .

--- app.py
+++ app.py
@@ -62,6 +62,11 @@ def order():
     return get_render_template('place_an_order.html', title="Оформление заказа")
 
 
+@app.route('/tmp')
+def tmp():
+    return get_render_template('tmp.html', title="Оформление заказа")
+
+
 # @app.route('/place_an_order', methods=['POST', 'GET'])
 # def place_an_order():
 #     if request.method == 'POST':
