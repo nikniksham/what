1b6fe939d75286678f1d81558c2d1f40c11565d9
Rjkzavr <kolya.toropof@gmail.com>
2022-09-24 14:35:32 +0300

    fix pagination

--- static/js/openPage.js
+++ static/js/openPage.js
@@ -2,17 +2,23 @@ let productsOnPage = 20;
 
 function openPage(page) {
     let products = document.getElementById('products');
-    console.log(products.children[0]);
-    console.log(products.children.length);
     for (let i=0; i < products.children.length; i++) {
         if ((i > productsOnPage * (page - 1)) && (i < productsOnPage * page + 1)) {
-            console.log(i);
             products.children[i].classList.remove('hidden');
         }
         else {
             products.children[i].classList.add('hidden');
         }
     }
+    let pagination = document.getElementById('pagination');
+    for (let i=0; i < pagination.children.length; i++) {
+        if (pagination.children[i].id == page) {
+            pagination.children[i].classList.add('active');
+        } else {
+            pagination.children[i].classList.remove('active');
+        }
+    }
+    window.scrollTo(0, 0);
 }
 
 openPage(1)
