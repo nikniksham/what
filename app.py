614110db5b6c63f91c1f04531930ec0cbacc3a0a
Niki <57631867+nikniksham@users.noreply.github.com>
2022-10-01 00:29:07 +0300

    add some functional to profile page

--- app.py
+++ app.py
@@ -131,11 +131,18 @@ def catalog():
 def profile():
     if current_user.is_anonymous:
         return redirect("/login")
+    return get_render_template('profile.html', title='Профиль')
+
+
+@application.route('/profile/profile-products')
+def profile_products():
+    if current_user.is_anonymous:
+        return redirect("/login")
     if ":" in current_user.orders:
         orders = get_list_orders_by_indexes(current_user.id, [int(key.split(":")[0]) for key in current_user.orders.split("|")])
     else:
         orders = []
-    return get_render_template('profile.html', title='Профиль', orders=orders)
+    return get_render_template('profile_products.html', title='Профиль', orders=orders)
 
 
 @application.route('/catalog/<string:cat>')
