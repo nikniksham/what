5f271ff83d068bcc8920931cfadca2b0ba696896
Niki <57631867+nikniksham@users.noreply.github.com>
2022-10-01 00:35:10 +0300

    fix

--- data/Inner/OrderAPI.py
+++ data/Inner/OrderAPI.py
@@ -179,7 +179,7 @@ def change_info(session, order, user_id, info, user):
             order.status = 1
             lch = order.max - order.current
             users[user_id] += lch
-            res = {"success": "заказ пошёл в обработку", "id": 2, "remains": min(info[0] - lch, order.max * 5)}
+            res = {"success": "заказ пошёл в обработку", "id": 2, "remains": min(info[0] - lch, order.max * 25)}
         else:
             users[user_id] += info[0]
             res = {"success": "пользователь увеличил кол-во заказа", 'id': 3}
